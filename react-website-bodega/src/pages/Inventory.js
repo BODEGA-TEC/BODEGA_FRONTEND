436fe432e56d141f8e01757b03007b23ff268031
Adrian Araya <44324449+Heutlett@users.noreply.github.com>
2023-09-26 20:47:47 -0600

    add: se agrega la conexion del inventario de equipo y se inicia la conexion del login

--- react-website-bodega/src/pages/Inventory.js
+++ react-website-bodega/src/pages/Inventory.js
@@ -1,79 +1,86 @@
 import '../App.css';
 import Footer from '../components/Footer/Footer';
 import Text from '../components/Text/Text';
-import {DataGrid} from '@mui/x-data-grid'
-import { Button } from '@mui/material';
-import data from './exampleJson.json'
-import { useState } from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import React, { useEffect, useState, useCallback } from 'react';
+
 const Inventory = () => {
-    
-    const [rows, setRowsValues] = useState([]);
-    {/*Definición de la estructura de las columnas en el Data Grid */}
-    const columns = [
-        {field: 'id',headerName: 'ID', width: 150,},
-        {field: 'descrip',headerName: 'Descripción', width: 150,},
-        {field: 'categoria',headerName: 'Categoría', width: 150,},
-        {field: 'marca',headerName: 'Marca', width: 150,},
-        {field: 'model',headerName: 'Modelo', width: 150,},
-        { field: 'serie', headerName: 'Serie', width: 130 },
-        {field: 'estado',headerName: 'Estado', width: 150,},
-        { field: 'num_activo', headerName: 'No. Activo', width: 130 },
-        {field: 'num_Tec',headerName: 'No. Activo Tec', width: 150,},
-        {field: 'observa',headerName: 'Observaciones', width: 160,},
-      ];
+  const [rows, setRowsValues] = useState([]);
+  const columns = [
+    {field: 'id',headerName: 'ID', width: 150,},
+    {field: 'descrip',headerName: 'Descripción', width: 150,},
+    {field: 'categoria',headerName: 'Categoría', width: 150,},
+    {field: 'marca',headerName: 'Marca', width: 150,},
+    {field: 'model',headerName: 'Modelo', width: 150,},
+    { field: 'serie', headerName: 'Serie', width: 130 },
+    {field: 'estado',headerName: 'Estado', width: 150,},
+    { field: 'num_activo', headerName: 'No. Activo', width: 130 },
+    {field: 'num_Tec',headerName: 'No. Activo Tec', width: 150,},
+    {field: 'observa',headerName: 'Observaciones', width: 160,},
+  ];
 
-    function createData(id, categoria, estado, descrip, num_activo, marca, model, num_Tec, serie, observa) {
-        return {id, categoria, estado, descrip, num_activo, marca, model, num_Tec, serie, observa};
+  function createData(id, categoria, estado, descrip, num_activo, marca, model, num_Tec, serie, observa) {
+    return { id, categoria, estado, descrip, num_activo, marca, model, num_Tec, serie, observa };
+  }
+
+  const fetchData = useCallback(async () => {
+    try {
+      const response = await fetch('http://localhost:5145/api/equipo');
+      if (!response.ok) {
+        throw new Error('Error al obtener los datos');
       }
-      
-    
-    {/*Cuando el botón "Actualizar" es clickeado, se recaban todos los datos */}
-    const handleUpdateTable = (event) =>{
-        let rowsAux = []
-        {/*Acá se ha de ingresar la función para recoger los objetos JSON de la
-        base de datos*/}
-        if (data.success === true){
-            data.data.forEach(
-                (device) =>{
-                    rowsAux = Object.assign([], rowsAux);
-                    rowsAux.push(
-                        createData(device.id, device.categoria.nombre, device.estado.descripcion, device.descripcion ,device.activoBodega, 
-                            device.marca, device.modelo, device.activoTec, device.serie, device.observaciones)
-                        );
-                }
-            );
-            setRowsValues(rowsAux)
-        }
-           
+      const data = await response.json();
+      if (data.success === true) {
+        const rowsAux = data.data.map((device) =>
+          createData(
+            device.id,
+            device.categoria.nombre,
+            device.estado.descripcion,
+            device.descripcion,
+            device.activoBodega,
+            device.marca,
+            device.modelo,
+            device.activoTec,
+            device.serie,
+            device.observaciones
+          )
+        );
+        setRowsValues(rowsAux);
+      }
+    } catch (error) {
+      console.error(error);
+      // Manejar errores aquí si es necesario
     }
+  }, []);
 
-    return(
-        <>
-            <div style={{marginLeft: '5%', marginTop: '50px'}}>
-            <Text text= "Buscar en inventario" text_style="text_title"/>
-            </div>
-            {/*Data grid. Esencialmente, una tabla especializada.*/}
-            <div style={{ marginLeft: '5%' , maxWidth: '90%'}}>
-                <DataGrid
-                    rows={rows}
-                    columns={columns}
-                    initialState={{
-                    pagination: {
-                        paginationModel: { page: 0, pageSize: 5 },
-                    },
-                    }}
-                    pageSizeOptions={[5, 10]}
-                    checkboxSelection
-                />
-            </div>
-            <div style={{marginBottom: '100px', marginTop: '20px', marginLeft: '5%'}}>
-            <Button variant="contained" onClick={handleUpdateTable}>Actualizar</Button>
-            </div>
-        <div style={{marginBottom: '0%'}}>               
-        <Footer/>
-        </div>
-        </>
-    );
-}
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  return (
+    <>
+      <div style={{ marginLeft: '5%', marginTop: '50px' }}>
+        <Text text="Buscar en inventario" text_style="text_title" />
+      </div>
+      <div style={{ marginLeft: '5%', maxWidth: '90%' }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 5 },
+            },
+          }}
+          pageSizeOptions={[5, 10]}
+          checkboxSelection
+        />
+      </div>
+      
+      <div style={{ marginBottom: '0%' }}>
+        <Footer />
+      </div>
+    </>
+  );
+};
 
 export default Inventory;
--- react-website-bodega/src/pages/LogIn.js
+++ react-website-bodega/src/pages/LogIn.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 import Footer from '../components/Footer/Footer';
 import Text from '../components/Text/Text';
@@ -7,71 +7,108 @@ import TextField from '@mui/material/TextField';
 import { Button } from '../components/Button/Button';
 
 export default function LogIn() {
+  const [token, setToken] = useState(''); // Estado para almacenar el token
+
+  // Función para manejar el envío del formulario
+  const handleLogin = async () => {
+    const apiUrl = 'http://localhost:5145/api/login';
+    const requestBody = {
+      carne: '0000000001',
+      clave: 'Asistente_1',
+    };
+
+    try {
+      const response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {'accept': 'text/plain',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestBody),
+      });
+
+      if (response.ok) {
+        
+        const data = await response.json();
+        const { data: tokenData } = data;
+
+
+
+        // Guardar el token en el estado
+        setToken(tokenData);
+
+        // Redirigir o realizar acciones adicionales según lo necesites
+      } else {
+        // Manejar errores de autenticación aquí
+      }
+    } catch (error) {
+      console.error('Error al iniciar sesión:', error);
+    }
+  };
+
   return (
     <>
-    <h1 className='log-in'>INICIAR SESIÓN</h1>
-    <div style={{textAlign: 'center', paddingTop: '50px', marginLeft: "20%", marginRight: "20%", marginBottom: "50px"}}>
+      <h1 className='log-in'>INICIAR SESIÓN</h1>
+      <div style={{ textAlign: 'center', paddingTop: '50px', marginLeft: "20%", marginRight: "20%", marginBottom: "50px" }}>
         <Text
-          text_style = 'text_title'
-          text = 'Ingresar'
+          text_style='text_title'
+          text='Ingresar'
         />
-    </div>
-    <div style={{marginTop: '50px', marginBottom: '50px', marginRight:'35%', marginLeft: '35%', textAlign:'center'}}>
-      <Box
-        component="form"
-        sx={{
-          backgroundColor: 'lightblue',
-          border: 1,
-          borderRadius: 10,
-          maxWidth: '100%',
-          '& .MuiTextField-root': { m: 1, width: '25ch' },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <div style={{marginBottom: '10px', marginTop:'10px'}}>
-          <TextField
-            
-            required
-            id="mail_login"
-            label="Correo"
-            defaultValue=""
-          />
-        </div>
-        <div style={{marginBottom: "25px"}}>
-          <TextField
-            required
-            id="pass_login"
-            label="Contraseña"
-            defaultValue=""
-          />
-        </div>
-        <Button 
-          className='btns'
-          buttonStyle= 'btn--secundary'
-          buttonSize = 'btn--large'
+      </div>
+      <div style={{ marginTop: '50px', marginBottom: '50px', marginRight: '35%', marginLeft: '35%', textAlign: 'center' }}>
+        <Box
+          component="form"
+          sx={{
+            backgroundColor: 'lightblue',
+            border: 1,
+            borderRadius: 10,
+            maxWidth: '100%',
+            '& .MuiTextField-root': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <div style={{ marginBottom: '10px', marginTop: '10px' }}>
+            <TextField
+              required
+              id="mail_login"
+              label="Correo"
+              defaultValue=""
+            />
+          </div>
+          <div style={{ marginBottom: "25px" }}>
+            <TextField
+              required
+              id="pass_login"
+              label="Contraseña"
+              defaultValue=""
+            />
+          </div>
+          <Button
+            className='btns'
+            buttonStyle='btn--secundary'
+            buttonSize='btn--large'
+            onClick={handleLogin} // Llama a la función al hacer clic en "Ingresar"
           >
             Ingresar
-        </Button>  
-      </Box>
-    </div>
-    <div style={{textAlign: 'center', paddingTop: '50px', marginLeft: "20%", marginRight: "20%", marginBottom: "150px"}}>
+          </Button>
+        </Box>
+      </div>
+      <div style={{ textAlign: 'center', paddingTop: '50px', marginLeft: "20%", marginRight: "20%", marginBottom: "150px" }}>
         <Text
-          text_style = 'text_title'
-          text = '¿No tienes cuenta?'
+          text_style='text_title'
+          text='¿No tienes cuenta?'
         />
         <a href='/create-acc'>
-            <Button 
+          <Button
             className='btns'
-            buttonStyle= 'btn--secundary'
-            buttonSize = 'btn--large'
-            >
+            buttonStyle='btn--secundary'
+            buttonSize='btn--large'
+          >
             Crear Cuenta
-            </Button>  
-        </a> 
-    </div>
-    
-    <Footer/>
+          </Button>
+        </a>
+      </div>
+      <Footer />
     </>
   );
 }
