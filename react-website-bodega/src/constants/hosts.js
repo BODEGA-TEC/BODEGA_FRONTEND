82d4f71abb1dabc20762540edce70f5efe6499f7
Adrian Araya <44324449+Heutlett@users.noreply.github.com>
2023-10-23 20:44:42 -0600

    add: add logs 2

--- react-website-bodega/src/constants/hosts.js
+++ react-website-bodega/src/constants/hosts.js
@@ -1,5 +1,5 @@
 const web_bodega_host = "http://172.21.6.223/api";
 const local_host = "http://localhost:5145/api";
-const current_host = local_host;
+const current_host = web_bodega_host;
 
 export {current_host}
