acd6cd54ae48903c519a85e4eafb7481ecc099dd
Adrian Araya <44324449+Heutlett@users.noreply.github.com>
2023-10-25 00:25:40 -0600

    add: changed new host

--- react-website-bodega/src/constants/hosts.js
+++ react-website-bodega/src/constants/hosts.js
@@ -1,4 +1,4 @@
-const web_bodega_host = "http://172.21.6.223/api";
+const web_bodega_host = "http://172.21.6.223:5050/api";
 const local_host = "http://localhost:5145/api";
 const current_host = web_bodega_host;
 
