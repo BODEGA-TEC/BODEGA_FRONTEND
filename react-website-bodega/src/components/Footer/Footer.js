85ff15b36e47608ad7bd8485bb2a1fed0d84cbe4
Kev1606 <kevincruz1606@estudiantec.cr>
2024-01-05 15:49:52 -0600

    Fix: Update Footer styling and content with a new design

--- react-website-bodega/src/components/Footer/Footer.js
+++ react-website-bodega/src/components/Footer/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Footer.css';
-// import { Button } from '../Button/Button';
 import { Link } from 'react-router-dom';
 
 function Footer() {
@@ -8,41 +7,13 @@ function Footer() {
     <div className='footer-container'>
       <div className='footer-link-wrapper'>
         <div className='footer-links'>
-            <div className='footer-link-items'>
-              <h2>Sobre Nosotros</h2>
-              <Link to='/services'>¿Cómo funciona?</Link>
-              <Link to='/services'>Ubicación</Link>
-              <Link to='/terms'>Términos de servicio</Link>
-            </div>
-      </div>
-      
-        <div className='footer-links'>
           <div className='footer-link-items'>
-            <h2>Contáctenos</h2>
-            <p>Teléfono:</p>
-            <p className='p-italic'>
-              (+506) 1234-5678
-            </p>
-            <p>Correo:</p>
-         
-            <p className='p-italic'>
-              contacto.bodegaie@gmail.com
-            </p>
+            <Link to='/'>
+              <a>Sobre Nosotros</a>
+            </Link>
           </div>
         </div>
-      </div>
-
-      <section className='social-media'>
-
-        <div className='social-media-wrap'>
-          <div className='footer-logo'>
-            <a className='social-logo' href="/">
-              Bodega Web <img className='bodega-logo' src='/logo.png' alt='logo' />
-            </a>
-          </div>
-
-          <small className='website-rights'> Copyright © {new Date().getFullYear()} hecho con ❤️ por el equipo de desarrollo de Bodega Web</small>
-          
+        <div className='footer-links'>
           <div className='social-icons'>
             <a className='social-icon-link facebook' target="_blank" rel="noreferrer"
               href="https://www.facebook.com/BodegaIETEC">
@@ -60,8 +31,16 @@ function Footer() {
             </a>
 
           </div>
+          <div className='footer-link-items'>
+            <p className='p-italic'>
+              (+506) 1234-5678
+            </p>
+            <p className='p-italic'>
+              contacto.bodegaie@gmail.com
+            </p>
+          </div>
         </div>
-      </section>
+      </div>
     </div>
   );
 }
