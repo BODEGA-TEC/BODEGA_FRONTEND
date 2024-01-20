4e8aaf7696a34b55d8ae053f7923dc6572c79b5c
Kev1606 <kevincruz1606@estudiantec.cr>
2024-01-20 16:48:30 -0600

    Feat: Validations and logic implemented

--- react-website-bodega/src/components/Login/Login.js
+++ react-website-bodega/src/components/Login/Login.js
@@ -1,14 +1,65 @@
 import React, { useState } from 'react';
 import './Login.css';
-import { TextField } from '@mui/material';
+import useAuth from "../../hooks/useAuth";
+import Alert from '@mui/material/Alert';
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import { login } from "../../services/AuthService";
 
 const Login = () => {
-  const [username, setUsername] = useState('');
+  const [userName, setUserName] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Handle login logic here (e.g., send a request to your server)
+  const [alertUserVisible, setAlertUserVisible] = useState(false);
+  const [alertUserMessage, setAlertUserMessage] = useState("");
+
+  const [alertPasswordVisible, setAlertPasswordVisible] = useState(false);
+  const [alertPasswordMessage, setAlertPasswordMessage] = useState("");
+
+  const { setAuth } = useAuth();
+
+  const TFUserName = (event) => {
+    setUserName(event.target.value);
+  }
+
+  const TFPassword = (event) => {
+    setPassword(event.target.value);
+  }
+
+  const checkInfo = (event) => {
+    if (userName === "" || password === "") {
+      if (userName === "") {
+        setAlertUserMessage("Campo de nombre vacío.");
+        setAlertUserVisible(true);
+      }
+      if (password === "") {
+        setAlertPasswordMessage("Campo de contraseña vacía.");
+        setAlertPasswordVisible(true);
+      }
+    } else {
+      handleSubmit(event);
+    }
+  }
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { user, roles, token } = await login({
+        carne: userName,
+        clave: password,
+      });
+
+      if (user) {
+        setAuth({ user, roles, token });
+        //navigate(from, { replace: true });
+      } else {
+        setAlertPasswordMessage("Información incorrecta.");
+        setAlertPasswordVisible(true);
+      }
+    } catch (err) {
+      setAlertPasswordMessage(err);
+      setAlertPasswordVisible(true);
+    }
   };
 
   return (
@@ -20,22 +71,45 @@ const Login = () => {
             type="text"
             id="username"
             className='input-field'
-            value={username}
+            value={userName}
             placeholder='Carné'
-            onChange={(e) => setUsername(e.target.value)}
-            style={{marginBottom: '10%', width: '250px'}}
+            onChange={(e) => { TFUserName(e) }}
+            style={{ marginBottom: '10%', width: '250px' }}
           />
+          <div>
+            {alertUserVisible && (
+              <Alert
+                severity="error"
+                onClose={() => setAlertUserVisible(false)}
+                sx={{ maxWidth: '250px', maxHeight: '100px', marginBottom: '10px', marginTop: '-20px'}}
+              >
+                {alertUserMessage}
+              </Alert>
+            )}
+          </div>
           <input
             type="password"
             id="password"
             value={password}
             className='input-field'
             placeholder='Contraseña'
-            onChange={(e) => setPassword(e.target.value)}
-            style={{marginBottom: '10%', width: '250px'}}
+            onChange={(e) => { TFPassword(e) }}
+            style={{ marginBottom: '10%', width: '250px' }}
           />
+          <div>
+            {alertPasswordVisible && (
+              <Alert
+                severity="error"
+                onClose={() => setAlertPasswordVisible(false)}
+                sx={{ maxWidth: '250px', maxHeight: '100px', marginBottom: '10px', marginTop: '-20px'}}
+              >
+                {alertPasswordMessage}
+              </Alert>
+            )}
+          </div>
         </div>
-          <button className='login-button'>Iniciar sesión</button>
+
+        <button className='login-button' onClick={(e) => {checkInfo(e)}}>Iniciar sesión</button>
       </div>
     </div>
   );
