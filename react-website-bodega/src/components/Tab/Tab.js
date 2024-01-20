a9c434cc20c180fa360ca99e5c1cafbd1390999e
Kev1606 <kevincruz1606@estudiantec.cr>
2024-01-20 15:13:48 -0600

    Feat: Update TabContent component in Tab.js and Services.js

--- react-website-bodega/src/components/Tab/Tab.js
+++ react-website-bodega/src/components/Tab/Tab.js
@@ -7,12 +7,35 @@ const Tab = ({ tabName, activeTab, handleTabClick }) => (
 );
 
 const TabContent = ({ activeTab }) => {
-  if (activeTab === 'Términos de servicio') {
+  if (activeTab === '¿Quiénes somos?') {
+    return <p>Bodega tiene la función de brindar un servicio de préstamo de equipos y componentes a los estudiantes de la Escuela de Electrónica del Tecnológico de Costa Rica. Gracias a esto, los estudiantes pueden tener acceso gratuito a equipo costoso, que es necesario para sus cursos.</p>;
+  } else if (activeTab === 'Términos de servicio') {
     return <p>Contenido de Servicios</p>;
   } else if (activeTab === '¿Cómo funciona?') {
-    return <p>Contenido de Cómo funciona</p>;
+    return <p>Para acceder a un préstamo, se han de completar los datos presentes en la boleta que se puede encontrar en esta misma página en la sección de Formularios. El profesor del curso por el cual se pidió el equipo, también tiene que firmar la boleta, dando fe de la necesidad del equipo. Luego, el estudiante se tiene que diriger a las instalaciones físicas, donde tendrá que retirar el equipo con los asistentes. Finalmente, completado el periodo de uso, se tendrá que devolver en las mismas condiciones a como se retiró, y seguir los pasos de devolución con los asistentes.</p>;
   } else if (activeTab === 'Ubicación') {
-    return <p>Contenido de Ubicación</p>;
+    return (
+      <div>
+        <p>Bodega está ubicada en la Sede Central del Tecnológico de Costa Rica en Cartago, específicamente, en el aula 113 del primer piso del edificio de Electrónica ubicado en el bloque K.</p>
+        <a href='https://www.google.com/maps/place/Escuela+de+Ingenier%C3%ADa+Electr%C3%B3nica.+TEC/@9.8547925,-83.907679,18.75z/data=!4m12!1m6!3m5!1s0x8fa0dff29640d73b:0xc11e19b85da8947f!2sTecnol%C3%B3gico+de+Costa+Rica!8m2!3d9.857247!4d-83.912313!3m4!1s0x0:0xff093cc7301877ce!8m2!3d9.8548824!4d-83.9071411?hl=es-419'>Ver ubicación en Maps</a>
+      </div>
+    )
+  } else if (activeTab === 'Horario') {
+    return (
+      <div>
+        <p>De Lunes a Viernes: 7:30 am a 8:30 pm</p>
+        <p>Sábados (Atención virtual 💻): 12:00 md a 4:30 pm</p>
+      </div>
+    );
+  } else if (activeTab === 'Servicios disponibles') {
+    return (
+      <div>
+        <p>- Préstamo de microcontroladores</p>
+        <p>- Préstamo de componentes (resistencias, capacitores...)</p>
+        <p>- Préstamo de equipo de medición (osciloscopios, testers...)</p>
+        <p>- Préstamo de integrados</p>
+      </div>
+    )
   }
   return null;
 };
