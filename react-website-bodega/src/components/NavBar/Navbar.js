7d283eb2a0cdfc31b3aa7ab5632aecbc81b9687d
Kev1606 <kevincruz1606@estudiantec.cr>
2024-01-26 14:10:36 -0600

    Feat: Navbar component to use useAuth hook and add conditional rendering for Admin menu

--- react-website-bodega/src/components/NavBar/Navbar.js
+++ react-website-bodega/src/components/NavBar/Navbar.js
@@ -2,11 +2,8 @@ import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./Navbar.css";
 import Controls from "../controls/Controls";
-import Popup from "../Popup";
-import { useTheme } from "@mui/material/styles";
-import { defaultPalette } from "../../config";
-import LogInBox from "./LogInBox";
-
+import useAuth from "../../hooks/useAuth";
+import { ROLES } from "../../utils/constants";
 
 function Navbar({ handleTabChange }) {
   const [click, setClick] = useState(false);
@@ -16,7 +13,7 @@ function Navbar({ handleTabChange }) {
   const closeMobileMenu = () => setClick(false);
   const navigate = useNavigate();
 
-  
+  const { hasRole, isLoggedIn } = useAuth();
 
   // Funtion that displays the button in mobile screens
   // when its required.
@@ -87,9 +84,35 @@ function Navbar({ handleTabChange }) {
                 </div>
               </div>
             </li>
+
+            {/* Condición para mostrar el menú de Administrador solo si el usuario está logueado y es administrador */}
+            {isLoggedIn() && hasRole(ROLES.ADMINISTRADOR) && (
+              <li className="nav-item">
+                <div className="dropdown">
+                  <Controls.Button
+                    className="dropbtn"
+                    text="Administrador"
+                    variant="outlined"
+                    style={{ color: "#FFF", borderColor: "#FFF" }}
+                  />
+                  <div className="dropdown-content">
+                    <Link to={"/admin/reportes"} className="nav-links" onClick={closeMobileMenu}>
+                      Reportes de Objetos No Devueltos
+                    </Link>
+                    <Link to={"/admin/registrar-asistentes"} className="nav-links" onClick={closeMobileMenu}>
+                      Registrar Asistentes
+                    </Link>
+                    <Link to={"/admin/mantenimiento-db"} className="nav-links" onClick={closeMobileMenu}>
+                      Mantenimiento de la base de datos
+                    </Link>
+                  </div>
+                </div>
+              </li>
+            )}
+
           </ul>
         </div>
-        
+
       </nav>
     </>
   );
